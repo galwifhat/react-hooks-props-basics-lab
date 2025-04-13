ec1c4c6ea35c50d4e9e64b1687c0d2158c2dab54
Ruth <cirurue@gmail.com>
2025-04-13 20:59:25 +0300

    props well done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(user) {
+  
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {!user.bio? null : user.bio==""? null : <p>{user.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links links={user.links} />
     </div>
   );
 }
